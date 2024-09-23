713185f910ea6054246565ee6e3c0b52ad1f1446
Fysiks <fysiks1@gmail.com>
2024-09-22 21:38:21 -0500

    Fix Server List Data Generation

--- Web/include/admin/admin_ban_add_online.php
+++ Web/include/admin/admin_ban_add_online.php
@@ -43,31 +43,32 @@
 	//get servers
 	$servers_array = array();
 	$resource = $mysql->query("SELECT * FROM ".$config->db_prefix."_serverinfo ORDER BY hostname ASC") or die ($mysql->error);
-	while($result = $resource->fetch_object()) {
-		$servers_list[] = $result->id;
-		$key = array_keys($servers_list);
-		$count = count($key);
-		
+	while( $result = $resource->fetch_object() )
+	{
 		//get some info
 		$server = new Rcon();
 		$server_address=explode(":",trim($result->address));
 		$server->Connect($server_address[0],$server_address[1], $result->rcon);
 		$infos = $server->Info();
 		$server->Disconnect();
-		for ($i=0; $i<$count; $i++) {
-			$servers_info = array(
-				"id"		=> $key[$i],
-				"hostname"	=> $result->hostname,
-				"address"	=> $result->address,
-				"rcon"		=> $result->rcon,
-				"map"         => $infos['map'],
-				"mod"        	=> $infos['mod'],
-				"os"		=> ($infos['os']=="l")?"Linux":"Windows",
-				"cur_players"	=> $infos['activeplayers'], 
-				"max_players"	=> $infos['maxplayers'],
-				"bot_players"	=> $infos['botplayers']
-			);
+
+		if( !$infos )
+		{
+			$infos = array();
 		}
+
+		$servers_info = array(
+			"id"          => count($servers_array),
+			"hostname"    => $result->hostname,
+			"address"     => $result->address,
+			"rcon"        => $result->rcon,
+			"map"         => array_key_exists('map', $infos) ? $infos['map'] : "",
+			"mod"         => array_key_exists('mod', $infos) ? $infos['mod'] : "",
+			"os"          => array_key_exists('os', $infos) ? ($infos['os']=="l")?"Linux":"Windows" : "",
+			"cur_players" => array_key_exists('activeplayers', $infos) ? $infos['activeplayers'] : "",
+			"max_players" => array_key_exists('maxplayers', $infos) ? $infos['maxplayers'] : "",
+			"bot_players" => array_key_exists('botplayers', $infos) ? $infos['botplayers'] : ""
+		);
 		$servers_array[] = $servers_info;
 	}
 
