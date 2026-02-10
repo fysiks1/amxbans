78ac3424f214453307d6d43ad558b2b0bb855e63
caxanga334 <10157643+caxanga334@users.noreply.github.com>
2026-02-10 19:05:19 -0300

    Fix Wrong Content Type
    Fixed amxbans.js.php not using application/javascript content type, which causes browsers to reject the file if X-Content-Type-Options: nosniff is set.

--- Web/templates/_js/amxbans.js.php
+++ Web/templates/_js/amxbans.js.php
@@ -1,4 +1,5 @@
 <?php require_once("../../include/init_session.php"); if(@$_SESSION["loggedin"]) { ?>
+<?php header('Content-Type: application/javascript'); ?>
 function LiveBanCopyVars (name,steamid,ip,userid) {
 	document.getElementById('player_name').value = name;
 	document.getElementById('player_uid').value = userid;
